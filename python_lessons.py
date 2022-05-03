a2d9c5b3c7b33502d8bac8b3b9e31cd7e213abdc
Saneks King <saneksking@gmail.com>
2022-05-03 16:34:42 +0700

    My lesson shpargalka

---
+++ python_lessons.py
@@ -0,0 +1,73 @@
+print('Операции с числами!'.center(5, '='))
+print(7 + 8)  # Операция сложения
+print('*' * 57)
+print(6 * 3) # Операция умножения
+print('*' * 57)
+print(6 / 3) # Операция деления
+print('*' * 57)
+print(4.0 + 5) # Операция сложения числа с точкой
+print('*' * 57)
+print(2 ** 5) # Возведение в степень
+print('*' * 57)
+print(3 ** (1 / 2)) # Возведение в степень с точкой
+print('*' * 57)
+print(20 // 3) # Частное от деления
+print('*' * 57)
+print(25 % 7) # Остаток от деления
+print('-' * 57)
+print('Операции со строками')
+print('This is string')
+print('I\'m developer')
+print('*' * 57)
+print('One\nTwo\nThree')
+print('''Four
+Five
+Six''')
+print('*' * 57)
+print('Python' + ' is' + ' awesome!')
+print('*' * 57)
+print('Great ' * 5)
+print('-' * 57)
+print('Операции с переменными')
+name = 'Saneks'
+x = 5
+print('*' * 57)
+print(x)
+print('*' * 57)
+print(x + 7)
+x = 'string'
+print('*' * 57)
+print(x)
+num = 5
+num += 6
+print('*' * 57)
+print(num)
+string = 'spam'
+string += 'egg'
+print('*' * 57)
+print(string)
+num1 = 5
+print(num1)
+print('*' * 57)
+print(num2:=int(input('Введите число: ')))
+print('*' * 57)
+print(num2)
+print('-' * 57)
+print('Операции с вводом и выводом')
+lastname = input('Введите свою фамилию: ')
+age = int(input('Введите ваш возраст: '))
+print(f'Ваша фамилия ' + lastname + ' и ваш возраст ' + str(age))
+print('-' * 57)
+print('Болево значения')
+print(4 == 4)
+print('*' * 57)
+print(1 == 2)
+print('*' * 57)
+print(5 != 4)
+print('*' * 57)
+print(5 >= 4)
+print('*' * 57)
+print(4 <= 3)
+print('*' * 57)
+print( 'Annie' < 'Anny')
+
