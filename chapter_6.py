b67d6dc43e6bd35f615cd544ae87473ed445662a
Saneks King <saneksking@gmail.com>
2022-06-16 16:29:13 +0700

    new

---
+++ chapter_6.py
@@ -0,0 +1,11 @@
+num = input('Enter 1 or 3: ')
+if num == '1':
+    print('1')
+elif num == '3':
+    print('3')
+else:
+    print('Incorect number')
+
+pets = ['cats', 'dogs', 'rabbits', 'hamsters']
+for myPets in pets:
+    print(myPets)
