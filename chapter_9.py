c9a0c514426f9ac01d451e0104e75a6bfb1f0ee9
saneksking <saneksking@gmail.com>
2022-06-28 14:43:06 +0700

    Continue OOP

--- chapter_9.py
+++ chapter_9.py
@@ -2,12 +2,12 @@
 
 class Staff:
     def __init__ (self, pPosition, pName, pPay):
-        self.position = pPosition
+        self._position = pPosition
         self.name = pName
         self.pay = pPay 
         print('Creating Staff object')
     def __str__(self):
-        return "Position = %s, Name = %s, Pay = %d"%(self.position, self.name, self.pay)
+        return "Position = %s, Name = %s, Pay = %d"%(self._position, self.name, self.pay)
     def calculatePay(self):
         prompt = '\nEnter number of hours worked for  %s: '%(self.name)
         hours = input(prompt)
@@ -15,3 +15,18 @@ class Staff:
         hourlyRate = input(prompt)
         self.pay = int(hours)*int(hourlyRate)
         return self.pay
+
+    @property
+    def position(self):
+        print('Geting method')
+        return seld._position
+    @position.setter
+    def position(self, value):
+        if value == 'Manager' or value == 'Basic':
+            self._position = value
+        else:
+            print('Not position!')
+
+
+staff = Staff('Basic', 'Saneks', 0)
+print(staff)
